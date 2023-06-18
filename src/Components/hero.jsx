f606368427b3e762c94a13000bf6f82bbb80f593
brickphone <mattis.hutt@gmail.com>
2023-06-18 18:54:17 +0200

    stuff

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -3,7 +3,14 @@ const Hero = (props) => {
   
   return (
     <main className="main-container">
-      <img src={props.img} alt="japan image"></img>
+        <img src={props.img} alt="japan image"></img>
+      <div className="right-container">
+        <p className="location">{props.location.country}</p>
+        <a href={props.location.link} className="link">View on google maps</a>
+        <h1 className="title">{props.title}</h1>
+        <p className="date">{props.date}</p>
+        <p className="description">{props.description}</p>
+      </div>
     </main>
   )
 }
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className="navbar-container">
       <img className="logo" src={earth}></img>
-      <h2 className="first-h2">my travel journal</h2>
+      <h2 className="first-h2">my travel journal.</h2>
     </div>
   )
 }
